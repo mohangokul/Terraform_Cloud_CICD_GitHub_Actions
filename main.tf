d8ad63ec8565b917b22c3c8149d56fc5f7224dcc
Neeharika <n.neeharikareddy@gmail.com>
2024-07-18 18:19:21 +0530

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,139 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "random_pet" "sg" {}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  root_block_device {
+    encrypted   = true
+  }
+  subnet_id              = aws_subnet.subnet.id
+  key_name               = var.key_name
+}
+
+variable "private_key_path"{
+  type = string
+  default = "./Neeharika_Terraform.pem"
+}
+variable "key_name" {
+  type = string
+ description = "key name"
+ default = "Neeharika_Terraform"
+}
+variable "region" {
+  type    = string
+  default = "eu-west-1"
+  description = "Name of the region to create resource"
+}
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+variable "subnet_address_space" {
+  default = "10.1.0.0/24"
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  vpc_id = aws_vpc.vpc.id
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["3.7.2.233/32"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["3.7.2.233/32"]
+  }
+
+  # outbound internet access
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.network_address_space
+  enable_dns_hostnames = "true"
+
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+}
+
+resource "aws_subnet" "subnet" {
+  cidr_block              = var.subnet_address_space
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+}
+
+# ROUTING #
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta-subnet" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.rtb.id
+}
+
+data "aws_availability_zones" "available" {}
+
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
+}
